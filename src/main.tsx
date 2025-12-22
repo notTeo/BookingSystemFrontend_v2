b9d17917ba5b86fc8d8fb7ebc34eb26f3dbeb850
Nick Teo <nikostheodosis05@gmail.com>
2025-12-22 12:07:06 +0200

    fixed theme bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import Router from "./Router";
 
 import "./styles/theme.css";
 
+const savedTheme = typeof localStorage !== "undefined" ? localStorage.getItem("theme") : null;
+const initialTheme = savedTheme === "light" ? "light" : "dark";
+document.documentElement.dataset.theme = initialTheme;
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router />
